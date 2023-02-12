74a15ad5e253b8a78872bfba9b9f83ea5f46bf8c
huutrong2307 <74829070+huutrong2307@users.noreply.github.com>
2023-02-12 16:13:40 +0700

    Custom error handler

--- jobs-api/middleware/error-handler.js
+++ jobs-api/middleware/error-handler.js
@@ -1,10 +1,26 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, please try again'
+    }
+    if (err.name === 'ValidationError') {
+        customError.status = 400;
+        customError.msg = Object.values(err.errors).map((item) => item.message).join(', ');
+    }
+    if (err.name === 'CastError') {
+        customError.status = 404;
+        customError.msg = `Not found with id: ${err.value}`;
+    }
+
+    if (err.code && err.code === 11000) {
+
+        customError.statusCode = 400;
+        customError.msg = `Duplicate value for entered for ${Object.keys(err.keyValue)}, please try another value`;
+    }
+    // return res.status(500).json({ msg: err });
+    return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 
 module.exports = errorHandlerMiddleware
