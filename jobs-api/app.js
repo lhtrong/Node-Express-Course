e16ff8334df9849f48db85c65733ff195770443e
huutrong2307 <74829070+huutrong2307@users.noreply.github.com>
2023-02-12 17:52:25 +0700

    Add extra security packages

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -8,11 +8,26 @@ const authMiddleware = require('./middleware/authentication')
 
 const connectDB = require('./db/connect');
 
+// Extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss');
+const rateLimiter = require('express-rate-limiter');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
